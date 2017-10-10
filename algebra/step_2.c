a2cef6b273f644c3bb53af1d997ba967d883a157
sandi-salamon <salamon.sandigmail.com>
2017-10-10 22:35:16 +0200

    ta bo pravi

--- algebra/step_2.c
+++ algebra/step_2.c
@@ -1,21 +1,17 @@
 #include <iostream>
 #include <cmath>
 
-float skalarni_produkt* (int vector_a*, int vector_b*, float phi*, float skalar*) {
-		*skalar = (*vector_a) * (*vector_b) * (*phi);
+int* skalarni_produkt (int* vector_a, int* vector_b) {
+		int skalar = 0;
+		 for (int i = 0; i < 3; i++) {
+			 skalar = skalar + (vector_a[3] * vector_b[3]);
 		return skalar;
 }
 int main () {
-	int vector_a[3] = {5,2,1};
-	int vector_b[3] = {3,1,1};
-	float skalar[3];
+	int vector_a[3] = {1,2,3};
+	int vector_b[3] = {1,0,0};
+	int produkt = skalarni_produkt(a,b);
 	 skalarni_produkt(vector_a, vector_b, skalar);
-	  std::cout<< " Skalarni produkt = "; 
-	   for(int i = 0; i < 3; ++i){
-		std::cout << result[i];
-		 if (i < 2){
-			std::cout << ", ";
-		}
-	 return skalar;
-   }
+	  std::cout<< " produkt " << std::endl;
+  return 0;
 }
