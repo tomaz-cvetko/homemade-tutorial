20f73d841ae8e9d128e5790eba1d4dcf9970736c
sandi-salamon <salamon.sandigmail.com>
2017-12-26 15:45:41 +0100

    to bi bil pa drugi nacin vendar ne znam odpraviti napak

--- algebra/step_3.cpp
+++ algebra/step_3.cpp
@@ -2,32 +2,32 @@
 
 class AdvancedVec {
 	public: 
-	   void set_values (int a1, int a2, int a3);
-	   void print_vector();
+	   vector(int, int, int);
+	   void set_values (int a1, int a2, int a3) {value = (a1, a2, a3);}
+	   int get_values () {return value;}
 
-    private: 
-       int x;
-       int y;
-       int z;	   
+	private: 
+		int value;
+		int x;
+		int y;
+		int z;	   
 };
 
-void AdvancedVec::set_values(int a1, int a2, int a3) {
+AdvancedVec::vector(int a1, int a2, int a3) {
 	x = a1;
 	y = a2;
 	z = a3;
-}
-
-void AdvancedVec::print_vector() {
-	std::cout << "Vector_a: " << x << y << z << std::endl;
+	value = (a1, a2, a3);
 }
 
 int main() {
 	AdvancedVec vector_a;
 	AdvancedVec vector_b;
+	vector_a.set_values(1, 2, 3);
+	vector_b.set_values(4, 5, 6);
 	
-	std::cout << "Vector_a: ";
-	
-	
+	std::cout << "Vector_a: " << vector_a.get_values() << std::endl;
+	std::cout << "Vector_b: " << vector_b.get_values() << std::endl;
 	
  return 0;	
 }
